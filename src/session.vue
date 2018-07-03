8e3ce97e9452f7446d790325ec91360805ef66e3
John Keiser <john@johnkeiser.com>
2018-07-03 09:01:06 -0700

    Add input line

--- src/session.vue
+++ src/session.vue
@@ -4,6 +4,10 @@
         <div class="input"><span class="commandline">{{command.command}}</span></div>
         <div class="output"><span v-for="output in command.output" :key="output.id" :class="output.stream">{{output.text}}</span></div>
     </div>
+    <div class="command">
+      <div class="input"><input type="text" id="commandline" width="100%" class="commandline" /></div>
+      <div class="output"></div>
+    </div>
   </div>
 </template>
 
