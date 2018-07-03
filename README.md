f65783e42efa3661206a3b49ab8387c97531735c
John Keiser <john@johnkeiser.com>
2018-07-03 07:48:41 -0700

    Basic mockup

---
+++ README.md
@@ -0,0 +1,14 @@
+Command Flow
+============
+
+Command Flow is a styleable command prompt window with collapsible output.
+
+Quick Start
+-----------
+
+1. [Install node](https://nodejs.org/en/download/current/)
+2. `git clone https://github.com/jkeiser/command-flow`
+3. `cd command-flow`
+4. `npm install`
+5. `npm start`
+
---
+++ src/session.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="session">
+    <div v-for="command in commands" :key="command.id" class="command">
+        <div class="input"><span class="commandline">{{command.command}}</span></div>
+        <div class="output"><span v-for="output in command.output" :key="output.id" :class="output.stream">{{output.text}}</span></div>
+    </div>
+  </div>
+</template>
+
+<script>
+const STDOUT = Symbol("stdout");
+const STDERR = Symbol("stderr");
+const STDIN  = Symbol("stdin");
+export default {
+  name: 'session',
+  data () {
+    return {
+      commands: [
+        {
+          command: "cd blah",
+          output: [
+          ]
+        },
+        {
+          command: "ls",
+          output: [
+            { stream: STDOUT, timestamp: Date.now(), text: "/hello.txt\n" },
+            { stream: STDOUT, timestamp: Date.now(), text: "/hello.csv\n" },
+          ]
+        }
+      ]
+    }
+  }
+}
+</script>
