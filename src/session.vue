1ce71c4a8601ae57102a71b1e1f2dec7d22eed53
John Keiser <john@johnkeiser.com>
2018-07-05 09:54:17 -0700

    Add instructions to command box; add command with error and one with a lot of output.

--- src/session.vue
+++ src/session.vue
@@ -5,7 +5,7 @@
         <div class="output"><span v-for="output in command.output" :key="output.id" :class="output.stream">{{output.text}}</span></div>
     </div>
     <div class="command">
-      <div class="input"><input type="text" id="commandline" width="100%" class="commandline" /></div>
+      <div class="input"><input type="text" id="commandline" width="100%" class="commandline" placeholder="Type a command here." /></div>
       <div class="output"></div>
     </div>
   </div>
@@ -21,17 +21,53 @@ export default {
     return {
       commands: [
         {
-          command: "cd blah",
+          command: "cd blagh",
+          exitCode: 1,
           output: [
+            { stream: STDERR, timestamp: Date.now(), text: "No such directory." }
           ]
         },
         {
+          command: "cd blah",
+          exitCode: 0,
+          output: []
+        },
+        {
           command: "ls",
+          exitCode: 0,
           output: [
-            { stream: STDOUT, timestamp: Date.now(), text: "/hello.txt\n" },
-            { stream: STDOUT, timestamp: Date.now(), text: "/hello.csv\n" },
+            { timestamp: Date.now(), text: "/hello.txt\n" },
+            { timestamp: Date.now(), text: "/hello.csv\n" },
           ]
-        }
+        },
+        {
+          command: "yes",
+          output: [
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+            { timestamp: Date.now(), text: "Y\n" },
+          ],
+          stderr: []
+        },
       ]
     }
   }
